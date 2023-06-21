1498db30eabeae090c57535df90bc80489bad083
Deborah3321 <nchabelengdeborah18@gmail.com>
2023-06-20 17:04:57 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _islower-show 1 is the input is a lowercase character. 
- * Another cases, shows 0
+ * _islower-show 1 is the input is a lowercase character.
+ * other cases, shows 0
  * @c: character to test
  * Return : 1 for lowercase character. 0 for the rest
  */
