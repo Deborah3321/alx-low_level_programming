e5d42b2009787f68eef85dc2db253110e07430cb
Deborah3321 <nchabelengdeborah18@gmail.com>
2023-06-20 17:14:25 -0700

    alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isalpha -shows 1 if the input is a letter. Another cases, return 0
+ * @c:character to test
  * Return: 1  for letters. 0 for the rest
  */
 int _isalpha(int c)
