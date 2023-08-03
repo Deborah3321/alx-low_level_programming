aa923049505bbc68ae3dd455743153fbdd03c0e8
Deborah3321 <nchabelengdeborah18@gmail.com>
2023-08-03 05:07:09 -0700

    crackme

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,17 +8,4 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int mask = 0, index;
-
-	index = n ^ m;
-
-	while (index > 0)
-	{
-		if (index & 1)
-		{
-			mask++;
-		}
-		index >>= 1;
-	}
-	return (mask);
-}
+	unsigned long int 
