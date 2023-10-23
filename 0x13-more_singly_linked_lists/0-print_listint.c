75351ee4a2287a6f05e9e55c23b0d6589806b7b9
Deborah3321 <nchabelengdeborah18@gmail.com>
2023-10-23 12:45:14 -0700

    check 5

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
+	if (!h)
 		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
+		nodes++;
 		h = h->next;
-		count++;
 	}
-
-	return (count);
+	return (nodes);
 }
